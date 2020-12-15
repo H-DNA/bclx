5dafafb767e500338aa30dc52df15e0014b6fa3c
Thanh-Dang Diep <diepda@lapnm10.in.nm.ifi.lmu.de>
2020-12-15 13:10:41 +0100

    Test

--- lock/ben/test.cpp
+++ lock/ben/test.cpp
@@ -17,6 +17,17 @@ int main(int argc, char *argv[])
   	std::cout << "(" << rank << "/" << nprocs << "): releasing lock" << std::endl;
   	l.release();
 
+	BCL::GlobalPtr<int> dang;
+	if (BCL::rank() == 1)
+	{
+		dang = BCL::alloc<int>(1);
+		printf("CP2: %lu: %u, %u\n", BCL::rank(), dang.rank, dang.ptr);
+	}
+
+	BCL::GlobalPtr<int> you;
+	you = BCL::alloc<int>(1);
+	printf("CP3: %lu: %u, %u\n", BCL::rank(), you.rank, you.ptr);
+
   	BCL::finalize();
   	return 0;
 }
