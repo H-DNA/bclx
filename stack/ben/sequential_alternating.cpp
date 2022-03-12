6c7fd52fdd817dcfa4b83951e66a5c1a3000b98f
Dang Diep <diepda@lapnm10.in.nm.ifi.lmu.de>
2022-03-12 22:10:17 +0100

    Add tracing data...

--- stack/ben/sequential_alternating.cpp
+++ stack/ben/sequential_alternating.cpp
@@ -59,14 +59,14 @@ int main()
 
         //tracing
         #ifdef  TRACING
-		uint64_t	total_elem_ru,
-				total_elem_rc,
-				total_succ_cs,
-				total_fail_cs,
-				total_succ_ea,
-				total_fail_ea;
-		double		node_time,
-				total_fail_time;
+		uint64_t	node_elem_ru,
+				node_elem_rc,
+				node_succ_cs,
+				node_fail_cs,
+				node_succ_ea,
+				node_fail_ea;
+		double		node_elapsed_time,
+				node_fail_time;
         	ta::na          na;
 
 		if (na.node_num == 1)
@@ -76,20 +76,35 @@ int main()
 					succ_ea, fail_ea,
 					elem_rc, elem_ru);
 
-		MPI_Reduce(&elem_ru, &total_elem_ru, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&elem_rc, &total_elem_rc, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&succ_cs, &total_succ_cs, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&fail_cs, &total_fail_cs, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&succ_ea, &total_succ_ea, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&fail_ea, &total_fail_ea, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&elapsed_time, &node_time, 1, MPI_DOUBLE, MPI_MAX, MASTER_UNIT, na.nodeComm);
-		MPI_Reduce(&fail_time, &total_fail_time, 1, MPI_DOUBLE, MPI_MAX, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&elem_ru, &node_elem_ru, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&elem_rc, &node_elem_rc, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&succ_cs, &node_succ_cs, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&fail_cs, &node_fail_cs, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&succ_ea, &node_succ_ea, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&fail_ea, &node_fail_ea, 1, MPI_UINT64_T, MPI_SUM, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&elapsed_time, &node_elapsed_time, 1, MPI_DOUBLE, MPI_MAX, MASTER_UNIT, na.nodeComm);
+		MPI_Reduce(&fail_time, &node_fail_time, 1, MPI_DOUBLE, MPI_MAX, MASTER_UNIT, na.nodeComm);
                 if (na.rank == MASTER_UNIT)
                         printf("[Node %d]%f (s), %f (s), %lu, %lu, %lu, %lu, %lu, %lu\n",
-					na.node_id, node_time, total_fail_time,
-					total_succ_cs, total_fail_cs,
-					total_succ_ea, total_fail_ea,
-					total_elem_rc, total_elem_ru);
+					na.node_id, node_elapsed_time, node_fail_time,
+					node_succ_cs, node_fail_cs,
+					node_succ_ea, node_fail_ea,
+					node_elem_rc, node_elem_ru);
+
+		if (na.node_num > 1)
+		{
+			uint64_t total_elem_ru = BCL::reduce(elem_ru, MASTER_UNIT, BCL::sum<uint64_t>{});
+			uint64_t total_elem_rc = BCL::reduce(elem_rc, MASTER_UNIT, BCL::sum<uint64_t>{});
+			uint64_t total_succ_cs = BCL::reduce(succ_cs, MASTER_UNIT, BCL::sum<uint64_t>{});
+			uint64_t total_fail_cs = BCL::reduce(fail_cs, MASTER_UNIT, BCL::sum<uint64_t>{});
+			uint64_t total_succ_ea = BCL::reduce(succ_ea, MASTER_UNIT, BCL::sum<uint64_t>{});
+			uint64_t total_fail_ea = BCL::reduce(fail_ea, MASTER_UNIT, BCL::sum<uint64_t>{});
+			if (BCL::rank() == MASTER_UNIT)
+				printf("[TOTAL]%lu, %lu, %lu, %lu, %lu, %lu\n",
+						total_succ_cs, total_fail_cs,
+						total_succ_ea, total_fail_ea,
+						total_elem_rc, total_elem_ru);
+		}
         #endif
 
 	BCL::finalize();
